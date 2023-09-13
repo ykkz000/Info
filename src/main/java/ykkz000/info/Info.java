fc4622a9f22456c27d1e038da66899b152deefc5
ykkz000 <ykkz000@163.com>
2023-09-13 09:00:54 +0800

    Force to render Display Name. Render blood bar.

---
+++ src/main/java/ykkz000/info/Info.java
@@ -0,0 +1,36 @@
+/*
+ * Info
+ * Copyright (C) 2023  ykkz000
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package ykkz000.info;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.fml.common.Mod;
+import org.slf4j.Logger;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(Info.MODID)
+public class Info {
+
+    // Define mod id in a common place for everything to reference
+    public static final String MODID = "info";
+    // Directly reference a slf4j logger
+    private static final Logger LOGGER = LogUtils.getLogger();
+    public Info() {
+        LOGGER.info("{} is loading...", MODID);
+    }
+}
---
+++ src/main/java/ykkz000/info/event/ClientForgeEventHandlers.java
@@ -0,0 +1,35 @@
+/*
+ * Info
+ * Copyright (C) 2023  ykkz000
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package ykkz000.info.event;
+
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.RenderNameTagEvent;
+import net.minecraftforge.eventbus.api.Event;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import ykkz000.info.Info;
+
+@Mod.EventBusSubscriber(modid = Info.MODID, value = Dist.CLIENT)
+public class ClientForgeEventHandlers {
+    @SubscribeEvent
+    public static void onRenderNameTag(RenderNameTagEvent event) {
+        // Always display name tags
+        event.setResult(Event.Result.ALLOW);
+    }
+}
